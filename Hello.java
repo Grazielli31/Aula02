372ee900c48a6f016463fb9a290d759241c5652c
Grazielli31 <graziellipereira46@gmail.com>
2025-02-28 19:39:44 -0300

    Adicionando novo método

--- Hello.java
+++ Hello.java
@@ -1,6 +1,10 @@
 public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        olaMundo();
+    }
 
-	    public static void main(String[] args) {
-	        System.out.println("Hello, World!");
-	    }
+    public static void olaMundo() {
+        System.out.println("Olá Mundo");
+    }
 }
